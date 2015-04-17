f4f19a47be3428046570392af9db636bbaa5ba42
JasonOlson15 <jason.s.olson@gmail.com>
2015-04-17 16:24:21 -0500

    Initial Push

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions in this file calculate the inverse of a matrix but leverage a 
+## cache so the calculation is only performed if it has not yet been done.
 
-## Write a short comment describing this function
+## ============================================================================
+## makeCacheMatrix
+## ============================================================================
+## Initializes the cache matrix and contains the functions necessary to get and 
+## set the data.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        
+        setResult <- function(mean) m <<- mean
+        
+        getResult <- function() m
+        
+        list(set = set, get = get,
+             setResult= setResult,
+             getResult = getResult)
 }
 
-
-## Write a short comment describing this function
+## ============================================================================
+## cacheSolve
+## ============================================================================
+## Returns the inverse of a matrix. First checks to see if that matrix is in 
+## cache and if so it pulls from there otherwise it is calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getResult()
+        
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get()
+        
+        m <- solve(data,)
+        
+        x$setResult(m)
+        
+        m
 }
